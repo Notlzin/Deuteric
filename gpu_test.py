d43f0fc5341911694162a8d478094352b3df7bb0
Neiteln <ne1.t3ln@gmail.com>
2025-11-20 09:05:11 +0800

    Removed gpu_test.py

--- gpu_test.py
+++
@@ -1,9 +0,0 @@
-# test_vgpu.py
-from src.vGPU import vGPU
-
-gpu = vGPU(20, 5)
-gpu.hookStdout()  # initialize internal GPUStdout
-
-# send strings directly through receive (which now uses GPUStdout internally)
-gpu._gpuStdout.vgpu.drawChar("Hello Deuteric!\n")
-gpu._gpuStdout.write("vGPU STDOUT only 😎")
